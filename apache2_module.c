852285fe7167aeea038d58d4e99fdee4abe91c12
sqs <qslack@qslack.com>
2010-07-08 11:46:27 -0700

    get actual pake_info struct pi_0 and pi_1, not hardcoded permit

--- apache2_module.c
+++ apache2_module.c
@@ -79,64 +79,26 @@ int parse_authorization_header(request_rec *r, auth_tcpcrypt_header_rec *resp)
  * Authorization header verification code
  */
 
-/* Gets pake info and stores it in `conf`. */
-static authn_status get_user_pake_info(request_rec *r, const char *user,
+/* Gets pake auth info for `user` (pi_0, pi_1, username) and stores it in `conf`. */
+static authn_status get_user_pake_info(request_rec *r, const char *username,
                                        auth_tcpcrypt_config_rec *conf)
 {
-    authn_status auth_result;
-    char *password;
-    authn_provider_list *current_provider;
-
-    current_provider = conf->providers;
-    do {
-        const authn_provider *provider;
-
-        /* For now, if a provider isn't set, we'll be nice and use the file
-         * provider.
-         */
-        if (!current_provider) {
-            provider = ap_lookup_provider(AUTHN_PROVIDER_GROUP,
-                                          AUTHN_DEFAULT_PROVIDER, "0");
-
-            if (!provider || !provider->get_realm_hash) {
-                ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
-                              "No Authn provider configured");
-                auth_result = AUTH_GENERAL_ERROR;
-                break;
-            }
-            apr_table_setn(r->notes, AUTHN_PROVIDER_NAME_NOTE, AUTHN_DEFAULT_PROVIDER);
-        }
-        else {
-            provider = current_provider->provider;
-            apr_table_setn(r->notes, AUTHN_PROVIDER_NAME_NOTE, current_provider->provider_name);
-        }
-
-
-        /* We expect the password to be md5 hash of user:realm:password */
-        auth_result = provider->get_realm_hash(r, user, conf->realm,
-                                               &password);
-
-        apr_table_unset(r->notes, AUTHN_PROVIDER_NAME_NOTE);
-
-        /* Something occured.  Stop checking. */
-        if (auth_result != AUTH_USER_NOT_FOUND) {
-            break;
-        }
-
-        /* If we're not really configured for providers, stop now. */
-        if (!conf->providers) {
-           break;
-        }
-
-        current_provider = current_provider->next;
-    } while (current_provider);
-
-    if (auth_result == AUTH_USER_FOUND) {
-        /* TODO2: get user pake info */
-        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "AUTH_USER_FOUND for user: '%s'", user);
+    authn_status authn_result;
+    
+    /* TODO: obviously un-hardcode */
+    if (strcmp(username, "jsmith") == 0) {
+        struct pake_info *pake = &conf->pake;
+        /* pake->username = username; */
+        /* pake->client.password = "jsmith"; */
+        /* TODO: in pake.c, this is currently also hardcoded -- change it there, too */
+        authn_result = AUTH_USER_FOUND;
+    } else {
+        authn_result = AUTH_USER_NOT_FOUND;
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+                      "get_user_pake_info: user not found: '%s'", username);
     }
 
-    return auth_result;
+    return authn_result;
 }
 
 
