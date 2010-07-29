9f73a3d2a0b79e135040b690239407afcbdba8b7
sqs <qslack@qslack.com>
2010-07-28 17:19:50 -0700

    AM hooks

--- pakebook/pakebook.py
+++ pakebook/pakebook.py
@@ -11,5 +11,15 @@ class index:
     def GET(self):
         return "Hello, world"
 
+def check_auth(handle):
+    if 'REMOTE_USER' in web.ctx.env:
+        user = web.ctx.env['REMOTE_USER']
+        web.header('X-Account-Management-Status',
+                   'active; name=%s; id=%s' % (user, user))
+    else:
+        web.header('X-Account-Management-Status', 'none')
+    handle()
+app.add_processor(check_auth)
+    
 if __name__ == "__main__":
     app.run()
