42d9e0352389c353f6d9a8549dee997f8c84c223
sqs <qslack@qslack.com>
2010-07-29 09:55:20 -0700

    start of user tracking code

--- pakebook/pakebook.py
+++ pakebook/pakebook.py
@@ -3,21 +3,32 @@ import web
 
 urls = (
     '/', 'index',
+    '/signup', 'signup',
+    '/login', 'login',
 )
 
 app = web.application(urls, globals())
 
 class index:
     def GET(self):
-        return "Hello, world"
+        if web.ctx.user:
+            return "Hello, %s." % web.ctx.user
+        else:
+            return "Not logged in."
+
+class signup:
+    def POST(self):
+        pass
 
 def check_auth(handle):
     if 'REMOTE_USER' in web.ctx.env:
         user = web.ctx.env['REMOTE_USER']
         web.header('X-Account-Management-Status',
                    'active; name=%s; id=%s' % (user, user))
+        web.ctx.user = user
     else:
         web.header('X-Account-Management-Status', 'none')
+        web.ctx.user = None
     return handle()
 app.add_processor(check_auth)
     
