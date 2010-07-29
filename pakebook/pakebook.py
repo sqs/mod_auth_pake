dc9a3be9b3dea2b64dbfcf0eb1e18672aa9b39b2
sqs <qslack@qslack.com>
2010-07-28 17:09:39 -0700

    basic pakebook .py and apache .htaccess

---
+++ pakebook/pakebook.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import web
+
+urls = (
+    '/', 'index',
+)
+
+app = web.application(urls, globals())
+
+class index:
+    def GET(self):
+        return "Hello, world"
+
+if __name__ == "__main__":
+    app.run()
