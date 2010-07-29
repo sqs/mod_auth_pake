05b265cd7d0556995f608df8c09ebab754b6198b
sqs <qslack@qslack.com>
2010-07-29 14:31:47 -0700

    404 if user not found

--- pakebook/pakebook.py
+++ pakebook/pakebook.py
@@ -5,7 +5,7 @@ urls = (
     '/', 'index',
     '/signup', 'signup',
     '/login', 'login',
-    '/~([a-z]+)', 'user',
+    '/~([a-zA-Z\d_@.-]+)', 'user',
 )
 
 app = web.application(urls, globals())
@@ -24,6 +24,9 @@ class signup:
 
 class user:
     def GET(self, username):
+        if username not in web.ctx.all_users:
+            web.ctx.status = '404 Not Found'
+            return "<h1>User '%s' not found</h1>" % username
         if username == web.ctx.user:
             return "<h1>Your profile</h1>" \
                    "<p>Your name: %s<br>Your email: %s@scs.stanford.edu</p>" % \
@@ -37,6 +40,8 @@ class user:
                    (username, username)
 
 def check_auth(handle):
+    web.ctx.all_users = [p[0] for p in map(str.split, open('.htpake').readlines())]
+    
     if web.ctx.env.get('REMOTE_USER'):
         user = web.ctx.env['REMOTE_USER']
         web.header('X-Account-Management-Status',
@@ -47,7 +52,7 @@ def check_auth(handle):
         web.ctx.user = None
 
     nav = "<p><a href='/'>Home</a> - Users: %s</p>" % \
-        ' '.join(["<a href='/~%s'>%s</a>" % (u,u) for u in [p[0] for p in map(str.split, open('.htpake').readlines())]])
+        ' '.join(["<a href='/~%s'>%s</a>" % (u,u) for u in web.ctx.all_users])
     if web.ctx.user:
         footer = "<hr><p>Logged in as <a href='/~%s'>%s</a>.</p>%s" % \
             (web.ctx.user, web.ctx.user, nav)
